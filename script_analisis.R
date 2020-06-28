9846e6bd3b145eae63331b51354046e9dba3fc27
sientifiko <socialscienceanime@gmail.com>
2020-06-28 00:03:38 -0400

    agregadas series de tiempo de democracia

--- script_analisis.R
+++ script_analisis.R
@@ -4,11 +4,16 @@ library(readxl)
 
 # ============= VISUALIZANDO DEMOCRACIA EN BOLIVIA ================
 
-# importamos el polity IV, y traemos columnas de interés
+# importamos datasets de democracia, y traemos columnas de interés
 bol.poliv <- read_xls("p5v2018.xls") %>% select(country, year, polity2) %>% 
   filter(country == "Bolivia")
 
-# grafiquemos la serie de tiempo
+bol.vdem <- read_rds("V-Dem-CY-Full+Others-v10.rds") %>% 
+  select(country_name, year, v2x_polyarchy, v2x_egaldem) %>%
+  filter(country_name == "Bolivia")
+
+
+# grafiquemos la serie de tiempo de polity iv
 ggplot(bol.poliv, aes(year, polity2)) +
   theme_classic() +
   geom_line(aes(alpha=1/5)) +
@@ -20,9 +25,20 @@ ggplot(bol.poliv, aes(year, polity2)) +
   labs(x="", y = "Ìndice de democracia Polity IV")
 
 
-# 
 
-
+# grafiquemos la serie de tiempo de de v-dem
+ggplot(bol.vdem, aes(year)) +
+  theme_classic() +
+  geom_line(aes(y= v2x_polyarchy, alpha=1/5)) +
+  geom_point(aes(y= v2x_polyarchy, color = "blue", shape = "Democracia Electoral")) +
+  geom_line(aes(y= v2x_egaldem, alpha=1/5)) +
+  geom_point(aes(y= v2x_egaldem, shape = "Democracia Igualitaria")) +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1), legend.position = "top") +
+  scale_y_continuous(limits = c(0,1)) +
+  scale_x_continuous(breaks = seq(1980, 2020, 2), limits = c(1980, 2020)) +
+  guides(color = "none", alpha = "none") +
+  labs(x= "", y="Nivel de democracia", shape = "", title = "Democracia en Bolivia", 
+       subtitle = "Datos del V-Dem")
 
 
 
